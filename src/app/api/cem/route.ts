a56caa7087776f4c6b46a583b8f8abc644cf673d
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2025-04-30 13:38:45 +0300

    test

--- src/app/api/cem/route.ts
+++
@@ -1,9 +0,0 @@
-import {NextResponse} from "next/server";
-
-export const GET = () => {
-    return NextResponse.json({
-        oai: process.env.OPENAI_API_KEY,
-        claude: process.env.ANTHROPIC_API_KEY,
-        env: process.env.NODE_ENV
-    })
-}
