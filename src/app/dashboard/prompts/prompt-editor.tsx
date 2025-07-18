543de59881190a078325e2e1aa699397a2114c95
Cem Yılmaz <c.yilmaz@depaulisystems.com>
2025-07-18 09:29:10 +0300

    Refactor `PromptEditor` with Markdown and HTML serialization improvements, added drag-and-drop support, and enhanced loading state handling.

--- src/app/dashboard/prompts/prompt-editor.tsx
+++ src/app/dashboard/prompts/prompt-editor.tsx
@@ -1,10 +1,14 @@
 import { EditorKit } from "@/components/editor/editor-kit";
 import { Editor, EditorContainer } from "@/components/editor/ui/editor";
 import { useDebouncedCallback } from "@/hooks/use-debounced-callback";
-import { LoaderCircle } from "lucide-react";
+import { MarkdownPlugin } from "@platejs/markdown";
 import { Plate, usePlateEditor } from "platejs/react";
+import { LoaderCircle } from "lucide-react";
 import * as React from "react";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import {HtmlPlugin, serializeHtml} from "platejs";
 
 interface PromptEditorProps {
     defaultValue: string;
@@ -15,48 +19,57 @@ interface PromptEditorProps {
 export const PromptEditor = ({defaultValue, onChange, isMarkdown = true}: PromptEditorProps) => {
     const editor = usePlateEditor({
         plugins: EditorKit,
+        skipInitialization: true
     });
 
     const [loading, setLoading] = useState(false);
     const defaultValuePer = useRef(defaultValue);
 
-    const handleChange = useCallback(async (editor) => {
-        //setLoading(true);
-        //const serializedValue = isMarkdown ? editor.api.markdown.serialize({value: editor.children}) : await serializeHtml(editor);
-        //console.log(serializedValue)
-        //onChange?.(serializedValue);
-        //setLoading(false);
-    }, [isMarkdown]);
+    const handleChange = async () => {
+        setLoading(true);
+        const serializedValue = editor.api.markdown.serialize({value: editor.children})
+        onChange?.(serializedValue);
+        setLoading(false);
+    };
 
     const debouncedHandleChange = useDebouncedCallback(handleChange, 800)
 
     useEffect(() => {
-        if (isMarkdown) {
-            editor.tf.setValue(editor.api.markdown.deserialize(defaultValuePer.current))
-        } else {
-            editor.tf.setValue(editor.api.html.deserialize({element: defaultValuePer.current}))
+        if(isMarkdown) {
+            editor.tf.init({
+                value: editor.getApi(MarkdownPlugin).markdown.deserialize(defaultValuePer.current),
+            });
+        }else{
+            editor.tf.init({
+                value: editor.getApi(HtmlPlugin).html.deserialize({element: defaultValuePer.current})
+            })
+            console.log(editor)
         }
     }, [defaultValuePer]);
 
     return (
-        <Plate
-            editor={editor}
-            onChange={({editor, value}) => {
-                //setLoading(true)
-                handleChange(editor)
-            }}
-        >
-            <EditorContainer>
-                <Editor
-                    placeholder="Type..."
-                />
-                {loading && (
-                    <div className="absolute right-2 bottom-2 z-10 rounded-full opacity-20">
-                        <LoaderCircle data-loader={true} className="animate-spin origin-center size-4"
-                                      aria-hidden="true"/>
-                    </div>
-                )}
-            </EditorContainer>
-        </Plate>
+        <DndProvider backend={HTML5Backend}>
+            <Plate
+                editor={editor}
+                onChange={({editor, value}) => {
+                    setLoading(true)
+                    debouncedHandleChange()
+                }}
+            >
+                <EditorContainer>
+                    <Editor
+                        defaultValue={defaultValue}
+                        onChange={(val) => console.log(val)}
+                        placeholder="Type..."
+                    />
+                    {loading && (
+                        <div className="absolute right-2 bottom-2 z-10 rounded-full opacity-20">
+                            <LoaderCircle data-loader={true} className="animate-spin origin-center size-4"
+                                          aria-hidden="true"/>
+                        </div>
+                    )}
+                </EditorContainer>
+            </Plate>
+        </DndProvider>
     );
 };
